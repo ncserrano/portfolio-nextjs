5292332f157d9dd19c2849390431bac533323533
Naraly <by.ncserrano@gmail.com>
2024-03-25 19:49:46 -0400

    added about page

---
+++ src/app/pages/about.js
@@ -0,0 +1,10 @@
+function AboutPage() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is the about page for our company.</p>
+        </div>
+    );
+}
+
+export default AboutPage;
